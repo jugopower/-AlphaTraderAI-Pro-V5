a55fc05a1a57c142143e84d72b4f6354a4c7d99c
jugopower <42241619+jugopower@users.noreply.github.com>
2026-07-12 17:12:22 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION="20260712-1630";
+const APP_VERSION="20260712-1705";
 self.addEventListener("install",event=>{self.skipWaiting();});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
