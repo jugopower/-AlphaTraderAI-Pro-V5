676e8f26f9bb0287c2b306aa642da4a5bd215e7f
jugopower <42241619+jugopower@users.noreply.github.com>
2026-07-13 12:03:22 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="build01642-20260713-1100";
+const CACHE="build0165-20260713-1200";
 const ASSETS=["./","./index.html"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
