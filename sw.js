7e2e45eb7dfbdf5fa5dbf7e52fbb317a616c1e38
jugopower <42241619+jugopower@users.noreply.github.com>
2026-08-04 08:07:46 +0800

    Add files via upload
    Build 028.1.4 Mode Separation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="build02813";
+const CACHE="build02814";
 const ASSETS=["./","./index.html"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
