f0e301e0a0fec5f7c5ef440c61fb05906615e0f5
jugopower <42241619+jugopower@users.noreply.github.com>
2026-08-04 22:52:17 +0800

    Add files via upload
    Build 028.2 Question Thumbnail Browser

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Build 028.1.12
-const CACHE="build028112";
+// Build 028.2
+const CACHE="build0282";
 const ASSETS=["./","./index.html","./human-ai-logo.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
